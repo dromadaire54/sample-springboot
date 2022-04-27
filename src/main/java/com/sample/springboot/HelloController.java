6beb33c3d6e58d9e551917e4085abed082cf2921
yberchi <yahia.berchi@lacooperativewelcoop.com>
2022-04-27 19:27:18 +0200

    Fix issue and update .gitignore file

--- src/main/java/com/sample/springboot/HelloController.java
+++ src/main/java/com/sample/springboot/HelloController.java
@@ -1,4 +1,4 @@
-package com.sample.springboot
+package com.sample.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
